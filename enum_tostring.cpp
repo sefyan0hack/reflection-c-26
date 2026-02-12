a7f0b1f309e0e6cedf410a1a2bd8e48e80298196
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-02-12 14:07:54 +0100

    un comment

--- enum_tostring.cpp
+++ enum_tostring.cpp
@@ -1,32 +1,32 @@
-// // g++ -std=c++26 -freflection
-// #include <print>
-// #include <string_view>
-// #include <meta>
+// g++ -std=c++26 -freflection
+#include <print>
+#include <string_view>
+#include <meta>
 
-// using namespace std::meta;
+using namespace std::meta;
 
-// // form p2996r13 - 3.6 Enum to String
-// template <typename E>
-//   requires (is_enum_type(^^E))
-// constexpr std::string_view enum_to_string(E value) {
-//   template for (constexpr auto e : std::define_static_array(std::meta::enumerators_of(^^E))) {
-//     if (value == [:e:]) {
-//       return identifier_of(e);
-//     }
-//   }
+// form p2996r13 - 3.6 Enum to String
+template <typename E>
+  requires (is_enum_type(^^E))
+constexpr std::string_view enum_to_string(E value) {
+  template for (constexpr auto e : std::define_static_array(std::meta::enumerators_of(^^E))) {
+    if (value == [:e:]) {
+      return identifier_of(e);
+    }
+  }
 
-//   return "<unnamed>";
-// }
+  return "<unnamed>";
+}
 
-// enum Color {
-//     Red, Green, Yellow
-// };
-// enum class State {
-//     ON, OFF
-// };
+enum Color {
+    Red, Green, Yellow
+};
+enum class State {
+    ON, OFF
+};
 
 int main()
 {
-    // std::println("{}", enum_to_string(Color::Red)); // Red
-    // std::println("{}", enum_to_string(State::ON)); // ON
+    std::println("{}", enum_to_string(Color::Red)); // Red
+    std::println("{}", enum_to_string(State::ON)); // ON
 }
