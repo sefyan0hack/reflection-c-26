7763ada4285f4b0cc9bc082421284d10c71b9b50
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-02-24 07:35:09 +0000

    extend run_tests to take namespaces

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -2,6 +2,7 @@
 #include <print>
 #include <meta>
 #include <cstring>
+#include <ranges>
 
 using namespace std::meta;
 
@@ -11,38 +12,44 @@ consteval auto has_annotation(info T, auto anno){
     return !annotations_of_with_type(T, ^^decltype(anno)).empty();
 }
 
-template<info NS>
-auto run_tests(){
-    template for (auto i = 1uz; constexpr info M : define_static_array(members_of(NS, access_context::current()))){
-        if constexpr (is_function(M)) {
-            std::println("#{} {} :: {} {}",
-                i++,
-                identifier_of(NS),
-                identifier_of(M),
-                has_annotation(M, ignore) ? "[ignored]" : ""
-            );
+template<std::meta::info... NSs>
+auto run_tests() -> void {
+    template for (auto nsc = 1uz; constexpr auto ns : {NSs...} ){
+        std::println("[{}] {}:", nsc, identifier_of(ns));
+        template for (auto tstc = 1uz; constexpr auto M : define_static_array(members_of(ns, access_context::current())) ){
+            if constexpr (is_function(M)) {
+                std::println("  [{}.{}] {} {}",
+                    nsc,
+                    tstc,
+                    identifier_of(M),
+                    has_annotation(M, ignore) ? "[ignored]" : ""
+                );
 
-            if(!has_annotation(M, ignore)){
-                [:M:]();
+                if(!has_annotation(M, ignore)){
+                    [:M:]();
+                }
             }
+            tstc++;
         }
+        nsc++;
     }
 }
 
-#define expect_eq(x, y)  do{if((x) != (y)) std::println("\t -> [ "#x" == "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
-#define expect_ne(x, y)  do{if((x) == (y)) std::println("\t -> [ "#x" != "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
-#define expect_streq(x, y)  do{if(std::strcmp(x, y) != 0) std::println("\t -> [ "#x" == "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
-#define expect_strne(x, y)  do{if(std::strcmp(x, y) == 0) std::println("\t -> [ "#x" != "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
-#define expect_that(statemnt)  do{if(!(statemnt)) std::println("\t -> [ "#statemnt" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
+#define expect_eq(x, y)  do{if((x) != (y)) std::println("\t-> [ "#x" == "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
+#define expect_ne(x, y)  do{if((x) == (y)) std::println("\t-> [ "#x" != "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
+#define expect_streq(x, y)  do{if(std::strcmp(x, y) != 0) std::println("\t-> [ "#x" == "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
+#define expect_strne(x, y)  do{if(std::strcmp(x, y) == 0) std::println("\t-> [ "#x" != "#y" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
+#define expect_that(statemnt)  do{if(!(statemnt)) std::println("\t-> [ "#statemnt" ] failed. {}:{}",__FILE__, __LINE__);}while(false);
 /////////////////////////////////////////////////////////
 
 auto add(int a, int b) -> int {
     return a + b;
 }
 
+//namespace as testsuite name
 namespace tests {
 
-    void add_random_tests() {
+    void add_random_tests() { // function as test case
         expect_eq(add(2, 2), 4);
         expect_ne(add(2, 2), 5);
         expect_strne("hh", "hh");
@@ -60,6 +67,14 @@ namespace tests {
     }
 }
 
+namespace tests2 {
+
+    void foo() {
+        expect_eq(add(2, 2), 4);
+        expect_ne(add(2, 2), 5);
+    }
+}
+
 int main() {
-    run_tests<^^tests>();
+    run_tests<^^tests, ^^tests2>();
 }
