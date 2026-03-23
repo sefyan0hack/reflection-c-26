3085cdbd9ffaa347a1e50d04aa076a6826c53fac
sefyan0hack <sefyan0hack@gmail.com>
2026-03-23 08:50:21 +0100

    Ensure a value exists and it is not another flag

--- cli_parse.cpp
+++ cli_parse.cpp
@@ -9,32 +9,40 @@
 
 using namespace std::meta;
 
-template <class Spec>
-auto parse(int argc, char** argv) -> Spec {
+template <class Opts>
+auto parse(int argc, char** argv) -> Opts {
   std::vector<std::string_view> cmdline(argv+1, argv+argc);
-  Spec opts;
+
+  Opts opts;
 
   constexpr auto ctx = std::meta::access_context::current();
-  template for (constexpr auto opt: std::define_static_array(nonstatic_data_members_of(^^Spec, ctx))) {
+  template for (constexpr auto opt: std::define_static_array(nonstatic_data_members_of(^^Opts, ctx))) {
 
     auto it = std::ranges::find_if(cmdline,
       [=](std::string_view arg){
         return (arg.starts_with("--") && arg.substr(2) == identifier_of(opt));
       });
-    
+
     if(it != cmdline.end())
     {
-      auto iss = std::ispanstream(it[1]);
-      if (iss >> opts.[:opt:]; !iss) {
-        std::print(stderr, "Failed to parse {:?} into option {} of type {}\n", it[1], display_string_of(opt), display_string_of(type_of(opt)));
+      if (it + 1 == cmdline.end() || (*(it+1)).starts_with("--")) {
+        std::print(stderr, "Missing value for option {}\n", display_string_of(opt));
         std::exit(EXIT_FAILURE);
       }
+
+      auto iss = std::ispanstream(*(it+1));
+      if (iss >> opts.[:opt:]; !iss) {
+        std::print(stderr, "Failed to parse {:?} into option {} of type {}\n", *(it+1), display_string_of(opt), display_string_of(type_of(opt)));
+        std::exit(EXIT_FAILURE);
+      }
+
+      cmdline.erase(it, it+2);
     }
   }
   return opts;
 }
 
-struct Args {
+struct Options {
   std::string name;
   int count = 1;
 };
@@ -52,7 +60,6 @@ auto print_struct(const auto& obj) -> void
 }
 
 int main(int argc, char** argv) {
-
-  auto opts = parse<Args>(argc, argv);
+  auto opts = parse<Options>(argc, argv);
   print_struct(opts);
 }
