7f364b7dc3a1b6ca82379947920b8d8a48e82c46
sefyan0hack <sefyan0hack@gmail.com>
2026-03-23 09:07:55 +0100

    use iostream

--- cli_parse.cpp
+++ cli_parse.cpp
@@ -1,11 +1,9 @@
-#include <string>
 #include <string_view>
 #include <ranges>
-#include <meta>
 #include <spanstream>
-#include <print>
-#include <array>
+#include <iostream>
 #include <vector>
+#include <meta>
 
 using namespace std::meta;
 
@@ -26,13 +24,13 @@ auto parse(int argc, char** argv) -> Opts {
     if(it != cmdline.end())
     {
       if (it + 1 == cmdline.end() || (*(it+1)).starts_with("--")) {
-        std::print(stderr, "Missing value for option {}\n", display_string_of(opt));
+        std::cerr << "Missing value for option " <<  display_string_of(opt) << std::endl;
         std::exit(EXIT_FAILURE);
       }
 
       auto iss = std::ispanstream(*(it+1));
       if (iss >> opts.[:opt:]; !iss) {
-        std::print(stderr, "Failed to parse {:?} into option {} of type {}\n", *(it+1), display_string_of(opt), display_string_of(type_of(opt)));
+        std::cerr << "Failed to parse `" << *(it+1) << "` to type " <<  display_string_of(type_of(opt)) << std::endl;
         std::exit(EXIT_FAILURE);
       }
 
@@ -47,19 +45,9 @@ struct Options {
   int count = 1;
 };
 
-auto print_struct(const auto& obj) -> void
-{
-  constexpr info T_ref = decay(type_of(^^obj));
-
-  constexpr auto ctx = access_context::current(); 
-  std::println("struct {} {{", display_string_of(T_ref));
-  template for (constexpr auto a : [:reflect_constant_array(nonstatic_data_members_of(T_ref, ctx)):]) {
-    std::println("  {} {} = {} ;", display_string_of(type_of(a)), identifier_of(a), obj.[:a:]);
-  }
-  std::println("}};");
-}
-
 int main(int argc, char** argv) {
   auto opts = parse<Options>(argc, argv);
-  print_struct(opts);
+
+  std::cout << "opts.name : " << opts.name << std::endl;
+  std::cout << "opts.count : " << opts.count << std::endl;
 }
